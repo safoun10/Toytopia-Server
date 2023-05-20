293ad7a1d0e09e1dcb448d71d3db92a03adc3f5b
Safoun <safoun.official@gmail.com>
2023-05-20 06:03:21 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",  (req , res) => {
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@safoundb.d2yflbg.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You are successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
     res.send("Toytopia is running");
 })
 
